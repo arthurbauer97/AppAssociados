c35e269701e0170ab906195624fd1adc9cd25624
Your Name <arthurbauer97@gmail.com>
2018-11-24 00:38:41 -0200

    Postagem Token Funcionando

--- AppAssociados.Domain/EstadoCivil.cs
+++ AppAssociados.Domain/EstadoCivil.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AppAssociados.Domain
 {
     public class EstadoCivil
     {
         public int id { get; set; }
+
+        [Required(ErrorMessage="Estado Civil Obrigatorio!",AllowEmptyStrings=false)]
+
         public string descricao { get; set; }
     }
 }
---
+++ AppAssociados.Repositories/Migrations/20181123021647_AddUsuario.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppAssociados.Repositories.Migrations
+{
+    public partial class AddUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    usuario = table.Column<string>(nullable: true),
+                    senha = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
